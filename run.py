12bbc75376a5d5fbc813e91f87361d49433058c8
vaibhav250904 <vaibhav.gupta@students.mq.edu.au>
2025-05-06 18:22:42 +1000

    final commit - vulnarabilities fixed - Virtual envrionment set up

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 from project import create_app
+import os 
 
 if __name__ == '__main__':
   app = create_app()
-  #debug_mode = os.getenv('FLASK_ENV', 'production') == 'development'
-  app.run(host='127.0.0.1', port=8001, debug=True) 
+  debug_mode = os.getenv('FLASK_ENV', 'production') == 'development'
+  app.run(host='127.0.0.1', port=8001, debug=debug_mode) 
 
