c341cf8fa1b4ba6c9599ceabdcb7fc8e7080e314
vaibhav250904 <vaibhav.gupta@students.mq.edu.au>
2025-05-06 18:05:37 +1000

    changed 0.0.0.0 to 127.0.0.1

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ from project import create_app
 
 if __name__ == '__main__':
   app = create_app()
-  debug_mode = os.getenv('FLASK_ENV', 'production') == 'development'
-  app.run(host='0.0.0.0', port=8001, debug=debug_mode) 
+  #debug_mode = os.getenv('FLASK_ENV', 'production') == 'development'
+  app.run(host='127.0.0.1', port=8001, debug=True) 
 
